94d80c031a655fd89733d10196b6e50a66498a41
Suraj Vadgama <contact@suninthesky.co.uk>
2018-06-25 22:54:10 +0100

    Set up build process

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # fusebox-static
+
+## Setup
+
+Prerequisites: [NodeJS](https://nodejs.org/), [Yarn](https://yarnpkg.com/)
+
+1. Clone repo
+2. `cd fusebox-static/`
+3. `yarn`
+
+## Development
+
+`npm run dev` to run development server.
+
+## Deployment
+
+1. `npm run build` to compile production assets.
+2. `surge dist/ fusebox.surge.sh`
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const RobotstxtPlugin = require('robotstxt-webpack-plugin').default;
+
+module.exports = {
+  entry: ['./src/assets/js/app.js', './src/assets/sass/app.sass'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'assets/bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+            presets: ['env']
+        }
+      },
+      {
+        test: /\.(sass|scss)$/,
+        loader: ExtractTextPlugin.extract({
+          use: [
+            { loader: 'css-loader', options: { minimize: true } },
+            { loader: 'sass-loader' }
+          ]
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: 'assets/bundle.css',
+      allChunks: true,
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new RobotstxtPlugin({
+      policy: [
+        {
+          userAgent: "*",
+          disallow: "/"
+        }
+      ]
+    })
+  ]
+};
