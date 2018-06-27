cdac9e21fa5ebdad321f9e336195e38d5e472d6e
Suraj Vadgama <contact@suninthesky.co.uk>
2018-06-27 22:05:02 +0100

    Update dashboard link

--- src/components/heading.vue
+++ src/components/heading.vue
@@ -2,7 +2,7 @@
   <div class="maxw1080 mxauto py25">
     <div v-show="data.loaded === 1" class="px15">
       <h1  class="h2 mb10">
-        {{ data.dashboard.team }} / <a href="/" class="snow bold">{{ data.dashboard.project }}</a>
+        {{ data.dashboard.team }} / <a :href="dashboardPath" class="snow bold">{{ data.dashboard.project }}</a>
       </h1>
 
       <div class="h3">{{ data.dashboard.description }}</div>
@@ -21,6 +21,11 @@ export default {
   },
   components: {
     Loading
+  },
+  data: function () {
+    return {
+      dashboardPath: `/?t=${this.data.params.t}`
+    }
   }
 }
 </script>
