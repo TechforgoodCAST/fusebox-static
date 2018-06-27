27da206754d196089c76cbc556ae77976ac995ea
Suraj Vadgama <contact@suninthesky.co.uk>
2018-06-27 22:03:06 +0100

    Responsive heading

--- src/components/heading.vue
+++ src/components/heading.vue
@@ -1,12 +1,14 @@
 <template>
-  <div class="maxw1080 mxauto py25 flex items-center">
-    <h1 v-show="data.loaded === 1" class="h2 px15">
-      {{ data.dashboard.team }} / <a href="/" class="snow bold">{{ data.dashboard.project }}</a>
-    </h1>
+  <div class="maxw1080 mxauto py25">
+    <div v-show="data.loaded === 1" class="px15">
+      <h1  class="h2 mb10">
+        {{ data.dashboard.team }} / <a href="/" class="snow bold">{{ data.dashboard.project }}</a>
+      </h1>
+
+      <div class="h3">{{ data.dashboard.description }}</div>
+    </div>
 
     <loading :loaded="data.loaded" wrapper="ml20"></loading>
-
-    <div class="h3 px15">{{ data.dashboard.description }}</div>
   </div>
 </template>
 
